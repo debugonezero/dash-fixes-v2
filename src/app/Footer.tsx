41fb2d2812d63f1d5fc1210ba7050543849e161a
Paulo Avila <onepaulo@icloud.com>
2025-10-22 14:18:18 -0700

    Replace Lucide social icons in footer with Font Awesome brand icons

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -1,5 +1,3 @@
-import { Youtube, Instagram, Facebook, MessageCircle, Star } from 'lucide-react';
-
 const Footer = () => {
   return (
     <footer className="py-8 px-4 sm:px-6 lg:px-8 bg-solarized-dark3 text-solarized-light">
@@ -17,7 +15,7 @@ const Footer = () => {
             className="hover:text-solarized-blue"
             aria-label="Visit our YouTube channel"
           >
-            <Youtube className="w-6 h-6" />
+            <i className="fa-brands fa-youtube w-6 h-6"></i>
           </a>
           <a
             href="https://www.instagram.com/dash.fixes/"
@@ -26,7 +24,7 @@ const Footer = () => {
             className="hover:text-solarized-blue"
             aria-label="Visit our Instagram page"
           >
-            <Instagram className="w-6 h-6" />
+            <i className="fa-brands fa-instagram w-6 h-6"></i>
           </a>
           <a
             href="https://www.facebook.com/profile.php?id=61580755575702"
@@ -35,7 +33,7 @@ const Footer = () => {
             className="hover:text-solarized-blue"
             aria-label="Visit our Facebook page"
           >
-            <Facebook className="w-6 h-6" />
+            <i className="fa-brands fa-facebook w-6 h-6"></i>
           </a>
           <a
             href="https://www.tiktok.com/@dashfixes"
@@ -44,7 +42,7 @@ const Footer = () => {
             className="hover:text-solarized-blue"
             aria-label="Visit our TikTok page"
           >
-            <MessageCircle className="w-6 h-6" />
+            <i className="fa-brands fa-tiktok w-6 h-6"></i>
           </a>
           <a
             href="https://www.yelp.com/biz/dash-fixes-pasadena"
@@ -53,7 +51,7 @@ const Footer = () => {
             className="hover:text-solarized-blue"
             aria-label="Visit our Yelp page"
           >
-            <Star className="w-6 h-6" />
+            <i className="fa-brands fa-yelp w-6 h-6"></i>
           </a>
         </div>
       </div>
